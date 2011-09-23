4cb9f13e354f5338f30fb0b43b4811ea6681ddf5
NoxNebula <patrick.kl@live.de>
2011-09-23 10:38:23 +0200

    Initial commit

---
+++ MinecraftStatus.class.php
@@ -0,0 +1,53 @@
+<?php
+class MinecraftStatus {
+	private $Socket, $Info;
+	public $Online, $MOTD, $CurPlayers, $MaxPlayers, $IP, $Port, $Error;
+	
+	public function __construct($IP, $Port = '25565') {
+		$this->IP = $IP;
+		$this->Port = $Port;
+		if($this->Socket = @stream_socket_client('tcp://'.$IP.':'.$Port, $ErrNo, $ErrStr, 1)) {
+			$this->Online = true;
+			
+			fwrite($this->Socket, "\xfe");
+			$Handle = fread($this->Socket, 2048);
+			$Handle = str_replace("\x00", '', $Handle);
+			$Handle = substr($Handle, 2);
+			$this->Info = explode("\xa7", $Handle); // Separate Infos
+			unset($Handle);
+			
+			if(sizeof($this->Info) == 3) {
+				$this->MOTD       = $this->Info[0];
+				$this->CurPlayers = (int)$this->Info[1];
+				$this->MaxPlayers = (int)$this->Info[2];
+				$this->Error      = false;
+			} else if(sizeof($this->Info) > 3) { // Handle error
+				$Temp = '';
+				for($i = 0; $i < sizeof($this->Info) - 2; $i++) {
+					$Temp .= ($i > 0 ? '§' : '').$this->Info[$i];
+				}
+				$this->MOTD = $Temp;
+				$this->CurPlayers = (int)$this->Info[sizeof($this->Info) - 2];
+				$this->MaxPlayers = (int)$this->Info[sizeof($this->Info) - 1];
+				$this->Error = 'Faulty motd or outdated script';
+			} else {
+				$this->MOTD       = false;
+				$this->CurPlayers = false;
+				$this->MaxPlayers = false;
+				$this->Error      = 'Unexpected error, cause may be an outdated script';
+			}
+		} else {
+			$this->Online = false;
+			$this->Error = 'Can not reach the server';
+		}
+	}
+	
+	public function Info() {
+		return array(
+			'MOTD'       => $this->MOTD,
+			'CurPlayers' => $this->CurPlayers,
+			'MaxPlayers' => $this->MaxPlayers
+		);
+	}
+}
+?>
