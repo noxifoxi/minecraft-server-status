1c839e55f57b51e49da05a5eaf17e0f4021e9226
NoxNebula <patrick.kl@live.de>
2011-09-23 15:24:32 +0200

    Added IP6 support and IP format errors

--- MinecraftStatus.class.php
+++ MinecraftStatus.class.php
@@ -2,19 +2,47 @@
 /**
  * Minecraft Server Status Class
  * @copyright	© 2011 Nox Nebula - Patrick Kleinschmidt
- * @website		https://github.com/NoxNebula/MC-Server-Status
- * @license		GNU Public Licence - Version 3
- * @author		Nox Nebula - Patrick Kleinschmidt
+ * @website	https://github.com/NoxNebula/MC-Server-Status
+ * @license	GNU Public Licence - Version 3
+ * @author	Nox Nebula - Patrick Kleinschmidt
  **/
 
 class MinecraftStatus {
 	private $Socket, $Info;
 	public $Online, $MOTD, $CurPlayers, $MaxPlayers, $IP, $Port, $Error;
 	
-	public function __construct($IP, $Port = '25565') {
+	public function __construct($IP, $Port = '25565') {	
 		$this->IP = $IP;
 		$this->Port = $Port;
-		if($this->Socket = @stream_socket_client('tcp://'.$IP.':'.$Port, $ErrNo, $ErrStr, 1)) {
+		
+		// Remove any protocols from serveraddress
+		if(preg_match('/(.*):\/\//', $this->IP)) {
+			$this->IP = preg_replace('/(.*):\/\//', '', $this->IP);
+		}
+		if(strpos($this->IP, '/') !== false) {
+			$this->IP = rtrim($this->IP, '/');
+			if(strpos($this->IP, '/') !== false) {
+				$this->Failed();
+				$this->Error = 'Unsupported IP/Domain format, no \'/\'s allowed';
+				return;
+			}
+		}
+		if(preg_match_all('/:/', $this->IP, $matches) > 1) {
+			unset($matches);
+			// IP6
+			if(strpos($this->IP, '[') === false && strpos($this->IP, ']') === false)
+				$this->IP = '['.$this->IP.']';
+		} else if(strpos($this->IP, ':') !== false) {
+			$this->Failed();
+			$this->Error = 'Unsupported IP/Domain format';
+			return;
+		}
+		
+		if($this->Socket = @stream_socket_client('tcp://'.$this->IP.':'.$Port, $ErrNo, $ErrStr, 1)) {
+			// If IP6 remove brackets
+			if(strpos($this->IP, '[') === 0 && strpos($this->IP, ']') === (strlen($this->IP) - 1))
+				$this->IP = trim($this->IP, '[]');
+			
 			$this->Online = true;
 			
 			fwrite($this->Socket, "\xfe");
@@ -30,7 +58,7 @@ class MinecraftStatus {
 				$this->CurPlayers = (int)$this->Info[1];
 				$this->MaxPlayers = (int)$this->Info[2];
 				$this->Error      = false;
-			} else if(sizeof($this->Info) > 3) { // Handle error
+			} else if(sizeof($this->Info) > 3) { // Handle error, Minecraft don't handle this.
 				$Temp = '';
 				for($i = 0; $i < sizeof($this->Info) - 2; $i++) {
 					$Temp .= ($i > 0 ? '§' : '').$this->Info[$i];
@@ -40,13 +68,12 @@ class MinecraftStatus {
 				$this->MaxPlayers = (int)$this->Info[sizeof($this->Info) - 1];
 				$this->Error      = 'Faulty motd or outdated script';
 			} else {
-				$this->MOTD       = false;
-				$this->CurPlayers = false;
-				$this->MaxPlayers = false;
+				$this->Failed();
 				$this->Error      = 'Unexpected error, cause may be an outdated script';
 			}
 		} else {
 			$this->Online = false;
+			$this->Failed();
 			$this->Error = 'Can not reach the server';
 		}
 	}
@@ -57,6 +84,12 @@ class MinecraftStatus {
 			'CurPlayers' => $this->CurPlayers,
 			'MaxPlayers' => $this->MaxPlayers
 		);
+	}
+	
+	private function Failed() {
+		$this->MOTD       = false;
+		$this->CurPlayers = false;
+		$this->MaxPlayers = false;
 	}
 }
 ?>
